1fc67d2a3c612c900eb63b08e62867b94bed64f3
Brijesh <brijeshdtt@gmail.com>
2017-08-17 00:35:00 +0530

    Version 5.1.1 - Minor changes

--- app/src/main/java/couplegoals/com/couplegoals/adapter/PostListAdapter.java
+++ app/src/main/java/couplegoals/com/couplegoals/adapter/PostListAdapter.java
@@ -1,8 +1,10 @@
 package couplegoals.com.couplegoals.adapter;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,7 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 import couplegoals.com.couplegoals.R;
+import couplegoals.com.couplegoals.database.DatabaseValues;
 import couplegoals.com.couplegoals.model.Post;
 
 /**
@@ -40,11 +43,16 @@ public class PostListAdapter extends ArrayAdapter<Post> {
         final View listViewItems = layoutInflater.inflate(R.layout.card_layout_view_daily_post,null,true);
 
         TextView textViewTodaysMessage = (TextView) listViewItems.findViewById(R.id.cardMessageToday);
+        CardView cardViewPostListDetails = (CardView) listViewItems.findViewById(R.id.cardViewPostListDetails);
+
         //ImageView cardImageTodayPost = (ImageView) listViewItems.findViewById(R.id.cardImageTodayPost);
 
         final Post postDetails = postDetailsList.get(position);
 
         textViewTodaysMessage.setText(Html.fromHtml(postDetails.getsTodayPostMessage()+"<b> posted by </b>" + postDetails.getsPostedBy()));
+        if (DatabaseValues.getUserDisplayName().equalsIgnoreCase(postDetails.getsPostedBy())){
+            cardViewPostListDetails.setCardBackgroundColor(context.getResources().getColor(R.color.cardview_shadow_start_color));
+        }
         //Picasso.with(context).load(postDetails.getsTodayPostImagePath()).into(cardImageTodayPost);
         return listViewItems;
     }
